7b858c5b268e32ba294eb98dea7d51184efc0c96
Kasim Necdet Percinel <kasim.n.percinel@nasa.gov>
2025-06-13 19:33:34 +0000

    keep traces now

--- playwright.config.ts
+++ playwright.config.ts
@@ -61,7 +61,7 @@ export default defineConfig({
     baseURL: "http://localhost:8080",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "on-first-retry"
+    trace: "retain-on-failure"
   },
   expect: {
     toHaveScreenshot: {
