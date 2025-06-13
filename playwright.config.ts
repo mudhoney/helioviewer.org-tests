eac60e212de2d8f043818e78e6158aba2825ad4e
Kasim Necdet Percinel <kasim.n.percinel@nasa.gov>
2025-06-13 19:00:29 +0000

    enable output for ci\

--- playwright.config.ts
+++ playwright.config.ts
@@ -31,14 +31,28 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: process.env.CI ? 'blob' : [
+  reporter: process.env.CI ? ['blob',
     [
-      "list",
-      {
-        printSteps: true
-      }
+        'list',
+        {
+            printSteps: true,
+        },
     ],
-    ["html"]
+ ] : [
+    [
+        'list',
+        {
+            printSteps: true,
+        },
+    ],
+    [
+        'html',
+        {
+            // attachmentsBaseURL: 'http://ec2-44-219-199-246.compute-1.amazonaws.com:8082' 
+            host: 'ec2-44-219-199-246.compute-1.amazonaws.com',
+            port: 8082,
+        },
+    ]
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
