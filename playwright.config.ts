7310ea47d98d260f712a6750d68c1b7604dc4165
Kasim Necdet Percinel <kasim.n.percinel@nasa.gov>
2025-06-24 16:01:58 +0000

    execute only 3d with trace on

--- playwright.config.ts
+++ playwright.config.ts
@@ -60,7 +60,8 @@ export default defineConfig({
     baseURL: "http://localhost:8080",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "retain-on-failure"
+    // trace: "retain-on-failure"
+    trace: "on"
   },
   expect: {
     toHaveScreenshot: {
