d85b4de6f9baff717070f228c74d177add2ec8aa
Kasim Necdet Percinel <kasim.n.percinel@nasa.gov>
2025-06-27 15:23:49 +0000

    fix conflict

--- playwright.config.ts
+++ playwright.config.ts
@@ -31,14 +31,24 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: process.env.CI ? 'blob' : [
+  reporter: process.env.CI ? [
+    ['blob'], 
     [
       "list",
       {
         printSteps: true
       }
     ],
-    ["html"]
+  ] : [
+    [
+        'list',
+        {
+            printSteps: true,
+        },
+    ],
+    [
+        'html'
+    ]
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
